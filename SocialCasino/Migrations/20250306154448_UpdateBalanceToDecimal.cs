6e9b56a2eb54074f70488c2ae450b3eacc48971f
Edwinas27 <edvinas.montvilas@gmail.com>
2025-03-06 18:38:18 +0200

    added gitignore

---
+++ SocialCasino/Migrations/20250306154448_UpdateBalanceToDecimal.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialCasino.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBalanceToDecimal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Balance",
+                table: "Users",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Balance",
+                table: "Users",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+    }
+}
